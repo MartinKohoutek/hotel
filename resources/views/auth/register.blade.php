18fc32721af21e79c416f4e65ba073fab17b189b
Martin Kohoutek <martinkohoutek@hotmail.com>
2023-10-09 08:15:13 +0200

    Frontend Register Page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,67 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+@extends('frontend.main_master')
+@section('main')
+<div class="hero medium-height jarallax" data-jarallax data-speed="0.2">
+    <img class="jarallax-img" src="{{ asset('frontend/img/hero_home_2.jpg') }}" alt="">
+    <div class="wrapper opacity-mask d-flex align-items-center justify-content-center text-center animate_hero" data-opacity-mask="rgba(0, 0, 0, 0.5)">
+        <div class="container">
+            <small class="slide-animated one">Luxury Hotel Experience</small>
+            <h1 class="slide-animated two">Sign Up</h1>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
+</div>
+<div class="container margin_120_95">
+    <div class="contact-form">
+        <div class="section-title text-center">
+            <span class="sp-color">Sign Up</span>
+            <h2 class="mb-5 text-center">Create an Account!</h2>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        <div id="message-contact"></div>
+        <form method="post" action="{{ route('register') }}" autocomplete="off">
+            @csrf
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-floating mb-4">
+                        <input class="form-control @error('name') is-invalid @enderror" type="text" id="name_contact" name="name" placeholder="Name">
+                        <label for="name_contact">Name</label>
+                        @error('name')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-floating mb-4">
+                        <input class="form-control" type="email" id="email_contact" name="email" placeholder="Email">
+                        <label for="email_contact">Email</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-floating mb-4">
+                        <input class="form-control" type="password" id="password" name="password" placeholder="Password">
+                        <label for="password">Password</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-floating mb-4">
+                        <input class="form-control" type="password" id="password_confirmation" name="password_confirmation" placeholder="Password">
+                        <label for="password_confirmation">Confirm Password</label>
+                    </div>
+                </div>
+            </div>
+           
+            <p class="mt-3">
+                <input type="submit" value="Submit" class="btn_1 outline w-100 d-block mx-auto">
+            </p>
+            <div class="col-12">
+                <p class="account-desc">Already have an account?<a href="{{ route('login') }}">Sign In</a>
+                </p>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
