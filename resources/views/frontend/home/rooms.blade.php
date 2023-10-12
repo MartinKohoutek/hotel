41c5e0c45da4787a8465b67e1951a598739e47d4
Martin Kohoutek <martinkohoutek@hotmail.com>
2023-10-12 07:21:16 +0200

    Frontend - Home Rooms Section

--- resources/views/frontend/home/rooms.blade.php
+++ resources/views/frontend/home/rooms.blade.php
@@ -1,43 +1,25 @@
+@php
+    $rooms = \App\Models\Room::latest()->limit(4)->get();
+@endphp
 <div class="title mb-3">
     <small data-cue="slideInUp">Luxury experience</small>
     <h2 data-cue="slideInUp" data-delay="200">Rooms & Suites</h2>
 </div>
 <div class="row justify-content-center add_bottom_90" data-cues="slideInUp" data-delay="300">
-    <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12">
+    @foreach ($rooms as $room)
+    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
         <a href="room-details.html" class="box_cat_rooms">
             <figure>
-                <div class="background-image" data-background="url({{ asset('frontend/img/rooms/1.jpg') }})"></div>
+                <div class="background-image" data-background="url({{ asset('upload/roomimg/'.$room->image) }})"></div>
                 <div class="info">
-                    <small>From $250/night</small>
-                    <h3>Junior Suite</h3>
+                    <small>From ${{ $room->price }}/night</small>
+                    <h3>{{ $room->roomtype->name }}</h3>
                     <span>Read more</span>
                 </div>
             </figure>
+            
         </a>
     </div>
-    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-        <a href="room-details.html" class="box_cat_rooms">
-            <figure>
-                <div class="background-image" data-background="url({{ asset('frontend/img/rooms/2.jpg') }})"></div>
-                <div class="info">
-                    <small>From $190/night</small>
-                    <h3>Deluxe Room</h3>
-                    <span>Read more</span>
-                </div>
-            </figure>
-        </a>
-    </div>
-    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-        <a href="room-details.html" class="box_cat_rooms">
-            <figure>
-                <div class="background-image" data-background="url({{ asset('frontend/img/rooms/3.jpg') }})"></div>
-                <div class="info">
-                    <small>From $240/night</small>
-                    <h3>Superior Room</h3>
-                    <span>Read more</span>
-                </div>
-            </figure>
-        </a>
-    </div>
+    @endforeach
     <p class="text-end"><a href="room-list-1.html" class="btn_1 outline mt-2">View all Rooms</a></p>
 </div>
