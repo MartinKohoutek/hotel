41f49c3c1be6a20f279077458e706f5a86a24931
Martin Kohoutek <martinkohoutek@hotmail.com>
2023-11-04 17:25:31 +0100

    Frontend - Show Blog Posts

--- resources/views/frontend/home/news.blade.php
+++ resources/views/frontend/home/news.blade.php
@@ -1,3 +1,6 @@
+@php
+    $posts = \App\Models\BlogPost::latest()->limit(3)->get();
+@endphp
 <div class="bg_white">
     <div class="container margin_120_95">
         <div class="title mb-3">
@@ -5,36 +8,19 @@
             <h2 data-cue="slideInUp" data-delay="200">News & Events</h2>
         </div>
         <div class="row justify-content-center home">
+            @foreach ($posts as $post)
             <div class="item col-xl-4 col-lg-6">
                 <a href="news-post.html" class="box_contents" data-cue="slideInUp" data-delay="300">
-                    <figure><img src="{{ asset('frontend/img/blog-1.jpg') }}" alt="" class="img-fluid"><em>11 Dec</em></figure>
+                    <figure><img src="{{ asset($post->post_image) }}" alt="" class="img-fluid"><em>{{ $post->created_at->format('d.m Y') }}</em></figure>
                     <div class="wrapper">
-                        <small>Travel<span></span></small>
-                        <h2>The vacation and travel Guide for experts in 2022</h2>
+                        <small>{{ $post->blogCategory->category_name }}<span></span></small>
+                        <h2>{{ $post->post_title }}</h2>
+                        <p style="color: black">{{ $post->short_description }}</p>
                         <em>Read more</em>
                     </div>
                 </a>
             </div>
-            <div class="item col-xl-4 col-lg-6">
-                <a href="news-post.html" class="box_contents" data-cue="slideInUp" data-delay="400">
-                    <figure><img src="{{ asset('frontend/img/blog-3.jpg') }}" alt="" class="img-fluid"><em>24 Dec</em></figure>
-                    <div class="wrapper">
-                        <small>Event<span></span></small>
-                        <h2>Yayoi Kusama: Infinity Mirror Rooms at Tate Modern</h2>
-                        <em>Read more</em>
-                    </div>
-                </a>
-            </div>
-            <div class="item col-xl-4 col-lg-6">
-                <a href="news-post.html" class="box_contents" data-cue="slideInUp" data-delay="500">
-                    <figure><img src="{{ asset('frontend/img/blog-2.jpg') }}" alt="" class="img-fluid"><em>21 Dec</em></figure>
-                    <div class="wrapper">
-                        <small>Restaurant<span></span></small>
-                        <h2>Best Local Restaurant in 2022</h2>
-                        <em>Read more</em>
-                    </div>
-                </a>
-            </div>
+            @endforeach
         </div>
         <!--/row -->
         <p class="text-end"><a href="news-1.html" class="btn_1 outline mt-2" data-cue="slideInUp" data-delay="600">View all News</a></p>
